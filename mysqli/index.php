9e179f8faff549265de7669172d98c4e08cdb8c7
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-02-01 14:18:58 -0500

    Added favicon and some minor modifications

--- mysqli/index.php
+++ mysqli/index.php
@@ -33,6 +33,7 @@ if ($searchTerm) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Computer Inventory</title>
+    <link rel="icon" type="image/png" href="../assets/favicon.ico">
     <link rel="stylesheet" href="../assets/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700;900&display=swap" rel="stylesheet">
 </head>
--- mysqli/insert.php
+++ mysqli/insert.php
@@ -69,6 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Computer</title>
+    <link rel="icon" type="image/png" href="../assets/favicon.ico">
     <link rel="stylesheet" href="../assets/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700;900&display=swap" rel="stylesheet">
 </head>
@@ -122,7 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 <div class="form-group">
                     <label for="added_by">Product Added By:</label>
-                    <input type="text" name="added_by" id="added_by" value="<?= htmlspecialchars($added_by) ?>">
+                    <input type="text" name="added_by" id="added_by" value="Kishan Kumar Das">
                     <span class="error"><?= $errors['added_by'] ?? '' ?></span>
                 </div>
 
